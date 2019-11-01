0a866cb45d41b58252c1c0ea808b7621ca1d6144
Josue Valencia <josue.valencia@domo.ec>
2019-11-01 10:55:37 +0100

    handling states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useReducer} from 'react'
 import axios from 'axios'
 import { Segment, Header, Form } from 'semantic-ui-react'
 
@@ -7,28 +7,51 @@ const App = () => {
   let unitFrom = 'EUR'
   let unitConv = 'USD'
 
+  const [currency, dispatch] = useReducer((state, {type, payload}) => {
+      let newState = {};
+      switch (type){
+        case 0:
+            newState = {...state, [type]:payload}
+          break;
+        default:
+            newState = {...state, [type]:payload}
+          break;
+      }
+      console.log(newState)
+      return newState
+    }, {ref: '', dest: '', unitFrom : '', unitConv:''});
+
+  const handleChange = (e, {value, name}) => {
+    dispatch({
+      type: name,
+      payload: value
+    });
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const currencies = await axios(
-        'https://openexchangerates.org/api/currencies.json',
-      );
-      setOptions(currencies.data);
-    };
-    fetchData();
+    fetchData(setOptions);
   }, []);
 
   return (
     <Segment style={{margin:'auto', maxWidth:'500px', marginTop:30}}>
-      <Header size='medium' style={{marginBottom:0}}>1.00  {unitFrom} entspricht</Header>
-      <Header size='large' color={'blue'} style={{marginTop:0}}>0.99 {unitConv}</Header>
+      <Header size='medium' style={{marginBottom:0}}>
+        {
+          !!  currency.ref
+          ?   `${currency.ref} ${unitFrom} entspricht`
+          :   'Fill the fieds' //TODO: add more logic into this output message
+        }
+      </Header>
+      <Header size='large' color={'blue'} style={{marginTop:0}}>
+        { !!  currency.dest &&  `${currency.dest} ${unitConv}` }
+      </Header>
       <Form>
         <Form.Group widths='equal'>
-          <Form.Input fluid />
-          <Form.Select fluid placeholder={'Currency Reference'} options={makeFormOpts(options)} />
+          <Form.Input  name='ref' value={currency.ref} onChange={handleChange} />
+          <Form.Select name='unitFrom' placeholder={'Currency Reference'} options={makeFormOpts(options)} onChange={handleChange} />
         </Form.Group>
         <Form.Group widths='equal'>
-          <Form.Input fluid />
-          <Form.Select fluid placeholder={'Currency destiny'} options={makeFormOpts(options)} />
+          <Form.Input  name='dest' value={currency.dest} onChange={handleChange} />
+          <Form.Select name='unitConv' placeholder={'Currency destiny'} options={makeFormOpts(options)} onChange={handleChange} />
         </Form.Group>
       </Form>
     </Segment>
@@ -38,3 +61,12 @@ const App = () => {
 export default App
 
 const makeFormOpts = (obj) => Object.keys(obj).map((value, i)=>({key:i, text:obj[value], value}))
+
+const fetchData = async (how) => {
+  const currencies = await axios(
+    'https://openexchangerates.org/api/currencies.json',
+  );
+  how(currencies.data);
+};
+
+//https://api.exchangeratesapi.io/latest?symbols=USD,GBP
